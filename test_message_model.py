10877cef3bb2616f01e5707314118f34dde91231
fan777 <fan777@gmail.com>
2021-05-02 20:35:06 -0700

    user following followed tests

--- test_message_model.py
+++ test_message_model.py
@@ -1,10 +1,58 @@
-import os
+"""Message model tests."""
 
+# run these tests like:
+#
+#    python -m unittest test_message_model.py
+
+
+import os
 from unittest import TestCase
 
-from app import app
-from models import db, Follows, Likes, User, Message
+from models import db, User, Message, Follows
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgres:///warbler_test'))
-app.config['SQLALCHEMY_ECHO'] = False
+# BEFORE we import our app, let's set an environmental variable
+# to use a different database for tests (we need to do this
+# before we import our app, since that will have already
+# connected to the database
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+
+# Now we can import app
+
+from app import app
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
+
+db.create_all()
+
+
+class MessageModelTestCase(TestCase):
+    """Test model for Messagse."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        User.query.delete()
+        Message.query.delete()
+        Follows.query.delete()
+
+        self.client = app.test_client()
+    
+    def test_message_model(self):
+        """Does basic model work?"""
+
+        u = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(u)
+        db.session.commit()
+
+        # User should have no messages & no followers
+        self.assertEqual(len(u.messages), 0)
+        self.assertEqual(len(u.followers), 0)
--- test_user_model.py
+++ test_user_model.py
@@ -30,7 +30,7 @@ db.create_all()
 
 
 class UserModelTestCase(TestCase):
-    """Test views for messages."""
+    """Test model for Users."""
 
     def setUp(self):
         """Create test client, add sample data."""
@@ -38,12 +38,22 @@ class UserModelTestCase(TestCase):
         User.query.delete()
         Message.query.delete()
         Follows.query.delete()
+        
+        u1 = User.signup('test1', 'test1@test.com', 'password', None)
+        u2 = User.signup('test2', 'test2@test.com', 'password', None)
+        db.session.commit()
+
+        self.u1 = u1
+        self.u2 = u2
 
         self.client = app.test_client()
+    
+    def tearDown(self):
+        db.session.rollback()
 
     def test_user_model(self):
         """Does basic model work?"""
-
+        
         u = User(
             email="test@test.com",
             username="testuser",
@@ -56,3 +66,15 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+    
+    def test_is_following(self):
+        self.u1.following.append(self.u2)
+        db.session.commit()
+        self.assertTrue(self.u1.is_following(self.u2))
+        self.assertFalse(self.u2.is_following(self.u1))
+
+    def test_is_followed_by(self):
+        self.u1.following.append(self.u2)
+        db.session.commit()
+        self.assertTrue(self.u2.is_followed_by(self.u1))
+        self.assertFalse(self.u1.is_followed_by(self.u2))
